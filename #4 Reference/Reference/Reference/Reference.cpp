9fa1ad778bec5fffddf17a6828c9f5611583f99e
RyenBZhao <fouxzhao@GMAIL.COM>
2021-07-14 20:53:02 +0800

    1-5

---
+++ #4 Reference/Reference/Reference/Reference.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int OriNum = 10;
+    int& Test_Reference = OriNum;
+    std::cout
+        << OriNum << std::endl
+        << Test_Reference << std::endl
+        << &OriNum << std::endl
+        << &Test_Reference << std::endl;
+    int&& RvalueNum = static_cast<int>(OriNum);//rvalue only contains value
+    std::cout
+        << OriNum << std::endl
+        << RvalueNum << std::endl
+        << &OriNum << std::endl
+        << &RvalueNum << std::endl;
+    //give more varible names (reference) to a value
+}
