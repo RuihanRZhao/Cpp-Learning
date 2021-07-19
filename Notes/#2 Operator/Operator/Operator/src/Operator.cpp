38c926b36dd648cccfe3bd25afeaba28924a4b77
Lzyl02 <87651207+Lzyl02@users.noreply.github.com>
2021-07-20 00:26:55 +0800

    update note for operator
    Made by Lzyl
    if you have any question, please contact Ryen on WeChat.
    
    Co-Authored-By: RyenBZhao <63040454+RyenBZhao@users.noreply.github.com>

---
+++ Notes/#2 Operator/Operator/Operator/src/Operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+int main() {
+	int a = 0;
+	a = 3 + 2;//summation 
+	a = 3 - 2;//subtraction
+	a = 3 * 2;//multiplication
+	a = 3 / 2;//division
+	a = 3 % 2;//modulus
+	a += 5;//a = a + 5
+	a -= 5;//a = a - 5
+	a *= 5;//a = a *5
+	a /= 5;//a = a / 5
+	a %= 5;//a = a % 5
+	a++;//While this line, a remains. When this line end, a = a + 1
+	++a;//While this line, a = a + 1. After this line, a remains.
+	a--;//While this line, a remains. When this line end, a = a - 1
+	--a;//While this line, a = a - 1. After this line, a remains.
+	return 0;
+}
