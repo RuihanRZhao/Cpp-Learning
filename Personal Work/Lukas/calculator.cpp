9075c7ac1a8a40db45325b0f64b0b46b5f39c6e2
RyenBZhao <fouxzhao@GMAIL.COM>
2021-07-21 07:46:51 +0800

    View Lukas's Work

--- Personal Work/Lukas/calculator.cpp
+++ Personal Work/Lukas/calculator.cpp
@@ -1,33 +1,38 @@
-//
-//  main.cpp
-//  calculator
-//
-//  Created by apple on 2021/7/19.
-//
-
+/*
+ Check the difference of the code.
+Make more necessary comments in your code, such as identify the function of a part of codes.
+ Well Done for your first programe
+ */
 #include <iostream>
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdbool.h>// useless here
 using namespace std;
 int main(){
     int FirstNum,SecondNum;
     char a;
-    cin>>FirstNum>>a>>SecondNum;
-    switch (a) {
-        case '+':
-            std::cout<<FirstNum<<"+"<<SecondNum<<"="<<FirstNum+SecondNum;
+    cin>>FirstNum>>a>>SecondNum;// in C type, you can write:    scanf("%d%c%d",&FirstNum,&a,&SecondNum);
+    switch (a) {//some system do not support const char like "+", use numbers of char can reduce bugs.
+        case '+':{
+            cout<<FirstNum<<"+"<<SecondNum<<"="<<FirstNum+SecondNum;// no need of std:: here
             break;
-        case '-':
-            std::cout<<FirstNum<<"-"<<SecondNum<<"="<<FirstNum-SecondNum;
+        }
+        case '-':{
+            cout<<FirstNum<<"-"<<SecondNum<<"="<<FirstNum-SecondNum;
             break;
-        case '*':
-            std::cout<<FirstNum<<"*"<<SecondNum<<"="<<FirstNum*SecondNum;
+        }
+        case '*':{
+            cout<<FirstNum<<"*"<<SecondNum<<"="<<FirstNum*SecondNum;
             break;
-        case '%':
-            std::cout<<FirstNum<<"%"<<SecondNum<<"="<<FirstNum%SecondNum;
-        default:
+        }
+        case '%':{
+            cout<<FirstNum<<"%"<<SecondNum<<"="<<FirstNum%SecondNum;
+            break;// DO NOT FORGET BREAK!!!!!
+        }
+        default:{
+            cout<<"Error!"<<endl;
             break;
-            return 0;
+        }
     }
-    }
+    return 0;//Make sure your "return 0;" has its right position
+}
 
--- Personal Work/Lukas/palindrome.cpp
+++ Personal Work/Lukas/palindrome.cpp
@@ -6,35 +6,27 @@
 //
 
 #include<iostream>
-#include<iomanip>
+#include<iomanip>//I am not sure why you use this. I hope that you did not copy others code. I choose to believe you this time. Also, you can try to make more surprise to me.
 using namespace std;
  
-int main()
-{
-    for(int z = 100;z < 10000;z++)
-    {
-        int a,b,c,d,e,f;
- 
-        if(z < 1000)
-        {
+int main(){//I think "{" should not take up a single line, it is a waste of space
+    for(int z = 100;z < 10000;z++){
+        int a,b,c,d,e,f;//Others cannot understand what is these variables for. Try to use more meaningful name.
+        if(z < 1000){//Why 1000?
             a = z / 100;
             b = z % 10;
-            if(a == b)
-            {
+            if(a == b){
                 cout << z << '\t';
             }
         }
- 
-        else
-        {
+        else{//this function cannot measure more digital numbers, think about it and impore your code. You can find a way to reverse the number and compare two numbers, try to reduce your variables. Limit 3 variables in the next version.
             a=z / 100;
             c=a / 10;
             d=a % 10;
             b=z % 100;
             e=b / 10;
             f=b % 10;
-            if(c==f&&d==e)
-            {
+            if(c==f && d==e){//try to use only one condition for this if()
                 cout << z << "\t";
             }
         }
