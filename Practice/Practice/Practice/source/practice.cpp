6efc4c0c42f46eea0f752f514f36fa6954b57ef8
RyenBZhao <fouxzhao@GMAIL.COM>
2021-11-25 22:56:49 +0000

    ！！！

---
+++ Practice/Practice/Practice/source/practice.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+#include<cmath>
+#include<stdio.h>
+
+int main() {
+	int n = 0;
+	std::cin>>n;
+	int triangle[n][n];
+	for (int i0 = 1; i0 <= n; i0++) {
+		for (int i1 = 1; i1 < i0-1; i1++) {
+
+		}
+	}
+
+
+
+
+
+	return 0;
+}
